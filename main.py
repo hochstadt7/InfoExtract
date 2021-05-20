9ee3a64490ce40a31bedd7defece3398bda6cd47
hochstadt7 <hochstadt@mail.tau.ac.il>
2021-05-20 21:24:16 +0300

    Fixing query code, processing actors page

--- main.py
+++ main.py
@@ -9,7 +9,8 @@ def extract_entity(s, start, end):
 
     start = s.index(start) + len(start)
     end = s.index(end, start)
-    return s[start:end]
+
+    return s[start:end] # the right index?
 
 # extract entity, relation and possibly second entity
 def extract_q_info(s):
@@ -18,62 +19,65 @@ def extract_q_info(s):
     # relation is saved as appears in the infobox on wiki, with lowercase and _ instead of space
     q_number=0
     if s[:2]=='Is':
+
         q_number=3
         relation = 'based_on'
-        entity=extract_entity(s,'is','based on')
+        entity=extract_entity(s,'Is ',' based on')
+        print(entity)
 
     elif s[:3]=='Who':
         if 'directed' in s:
             q_number = 1
             relation='directed_by'
-            entity=extract_entity(s,'directed','?')
+            entity=extract_entity(s,'directed ','?')
+
         elif 'produced' in s:
             q_number = 2
             relation = 'produced_by'
-            entity = extract_entity(s, 'produced', '?')
+            entity = extract_entity(s, 'produced ', '?')
         else:
             q_number = 6
             relation = 'starring'
-            entity = extract_entity(s, 'starred in', '?')
+            entity = extract_entity(s, 'starred in ', '?')
 
     elif s[:3]=='Did':
         q_number = 7
         relation='starring'
-        entity = extract_entity(s, 'Did', 'star in')
-        entity2=extract_entity(s, 'star in', '?')
+        entity2 = extract_entity(s, 'Did ', ' star in')
+        entity=extract_entity(s, 'star in ', '?')
 
     elif s[:3]=='How':
         if 'long' in s:
             q_number = 5
             relation = 'running_time'
-            entity =extract_entity(s, 'is', '?')
+            entity =extract_entity(s, 'is ', '?')
         elif 'based_on' in s:
             q_number = 10
             relation='based_on'
         elif 'starring' in s:
             q_number = 11
             relation='starring'
-            entity=extract_entity(s,'starring', 'won')
+            entity=extract_entity(s,'starring ', ' won')
         else:
             q_number = 12
             relation='occupation'
-            entity=extract_entity(s,'many','are')
-            entity2=extract_entity(s,'also','?')
+            entity=extract_entity(s,'many ',' are')
+            entity2=extract_entity(s,'also ','?')
 
     elif s[:4]=='When':
         if 'born' in s:
             q_number = 8
             relation='born'
-            entity=extract_entity(s,'was','born')
+            entity=extract_entity(s,'was ',' born')
         else:
             q_number = 4
             relation='released_date'
-            entity=extract_entity(s,'was','released')
+            entity=extract_entity(s,'was ',' released')
 
     elif s[:4]=='What':
         q_number = 9
         relation='occupation'
-        entity=extract_entity(s,'of','?')
+        entity=extract_entity(s,'of ','?')
 
     else:
         print('Wrong question format')
@@ -83,48 +87,52 @@ def extract_q_info(s):
 
 # get answers using our ontology
 def query(q_number,relation,entity,entity2):
-    relation=relation.replace(' ', '_')
+
     entity = entity.replace(' ', '_')
     entity2=entity2.replace(' ', '_')
+    g=rdflib.Graph()
+    g.parse("ontology.nt", format="nt")
 
     if q_number==1 or q_number==2 or q_number==4 or q_number==5 or q_number==6 or q_number==8 or q_number==9:
-        output=g.query("select ?a where {<"+PREFIX + entity + "> <"+PREFIX + relation + "> ?a .}")
+
+        res=g.query("select ?a where {<"+PREFIX +'/'+ entity + "> <"+PREFIX +'/'+ relation + "> ?a .}")
+
         # produce list of answers
-        output=', '.join([result[1][len(PREFIX):].replace('_',' ') for result in output])
+        output=', '.join([result[0][len(PREFIX)+1:].replace('_',' ') for result in res])
 
     elif q_number==3:
-        output=g.query("select ?a where {<"+PREFIX + entity + "> <"+PREFIX + relation + "> ?a .}") # how do we know if type is book? need to add to ontology too?
+
+        res=g.query("select (COUNT(?a) as ?CNT) where {<"+PREFIX +'/'+ entity + "> <"+PREFIX +'/'+ relation + "> ?a .}") # how do we know if type is book? need to add to ontology too?
+        output=list(res)[0]
+        if output:
+            output='Yes'
+        else:
+            output='No'
 
     elif q_number==7:
-        output=g.query("select (COUNT(?s) as ?CNT) where {?s <"+PREFIX + relation + "> <"+PREFIX +entity+"> .}")
 
-        temp = output
-        for result in temp: # it will take only one iteration, this is the easiest way i found to extract the actual count
-            output = result[0]
-
-        if output=='0':
-            output='No'
-        else:
+        res=g.query("ASK {<"+PREFIX +'/'+ entity + "> <"+PREFIX +'/'+ relation + "> <"+PREFIX+'/'+entity2+ "> .}")
+        output=list(res)[0]
+        if output:
             output='Yes'
+        else:
+            output='No'
 
     elif q_number==10:
-        output=g.query("select ?a where {<"+PREFIX + entity + "> <"+PREFIX + relation + "> ?a .}") # how do we know if type is book? need to add to ontology too?
+        res=g.query("select (COUNT(?a) as ?CNT) where {?a <"+PREFIX +'/'+ relation + "> <"+PREFIX +'/'+ entity + "> .}") # how do we know if type is book? need to add to ontology too?
+        output = list(res)[0]
 
     elif q_number==11:
-        output=g.query("select (COUNT(?s) as ?CNT) where {?s <"+PREFIX + relation + "> ?a ."\
-                       "?a <"+PREFIX+"awards> <"+PREFIX+"academy_awards> ."
+        res=g.query("select (COUNT(?s) as ?CNT) where {?s <"+PREFIX +'/'+ relation + "> ?a ."\
+                       "?a <"+PREFIX+'/'+"award" +"> ?b ."
         )
-        temp=output
-        for result in temp:
-            output = result[0]
+        output=list(res)[0]
 
     elif q_number==12:
-        output = g.query("select (COUNT(?s) as ?CNT) where {?s <"+PREFIX + relation + "> <"+PREFIX +entity+ "> ." \
-                            "?s <"+PREFIX + relation + "> <"+PREFIX +entity2+ "> ."
+        res = g.query("select (COUNT(?s) as ?CNT) where {?s <"+PREFIX +'/'+ relation + "> <"+PREFIX +'/'+entity+ "> ." \
+                            "?s <"+PREFIX +'/'+ relation + "> <"+PREFIX +'/'+entity2+ "> ."
                          )
-        temp = output
-        for result in temp:
-            output = result[0]
+        output=list(res)[0]
 
     else:
         output=None
@@ -136,8 +144,9 @@ if __name__ == "__main__":
 
     input=sys.argv[1]
     if input=='question':
+
         q_number,relation,entity,entity2 = extract_q_info(sys.argv[2])
         query(q_number,relation,entity,entity2)
 
     elif input=='create':
-        build_ontology(g)
+        build_ontology()
--- ontology.py
+++ ontology.py
@@ -6,9 +6,10 @@ import rdflib
 PREFIX = "http://example.org"
 seed_address='https://en.wikipedia.org/wiki/List_of_Academy_Award-winning_films'
 relation_ontologies={}
+g = rdflib.Graph()
 
 # add triple to our ontology
-def add_triple(ont_name,xpath_result,key,g):
+def add_triple(ont_name,xpath_result,key):
 
     result=xpath_result[0].replace(" ","_")
 
@@ -17,8 +18,113 @@ def add_triple(ont_name,xpath_result,key,g):
     ont_val = rdflib.URIRef(f'{PREFIX}/{decode_string}')
     g.add((ont_name, relation_ontologies[key], ont_val))
 
+def check_human(ont_name,a,str1,str2):
+
+    td = a[0].xpath("./tbody/tr[./th//text()='"+str1+"']/td")
+
+    if td != []:
+        if str1=='Born':
+            bdays=td[0].xpath(".//span[contains(@class,'bday')]")
+            if not bdays:
+
+                return
+            else:
+                for bday in bdays:
+                    add_triple(ont_name,bday.xpath("./text()"),str2)
+                return
+        elif str1=='Awards':
+            g.add((ont_name, relation_ontologies['ont_award'],rdflib.URIRef(f'{PREFIX}/Yes') )) # Does award, we don't really care on which.
+            return
+
+        lis = td[0].xpath("./div/ul/li")
+        for li in lis:
+
+            iss = li.xpath(".//i")
+            for i in iss:
+
+                links = i.xpath("./a")
+                if not links:
+                    add_triple(ont_name, i.xpath("./text()"), str2)
+                else:
+                    for link in links:
+                        add_triple(ont_name, link.xpath("./text()"), str2)
+
+            if not iss:
+                links = li.xpath("./a")
+                if not links:
+                    add_triple(ont_name, li.xpath("./text()"), str2)
+                else:
+                    for link in links:
+                        add_triple(ont_name, link.xpath("./text()"), str2)
+
+        if not lis:
+            lis = td[0].xpath("./ul/li")
+            for li in lis:
+
+                iss = li.xpath(".//i")
+                for i in iss:
+
+                    links = i.xpath("./a")
+                    if not links:
+                        add_triple(ont_name, i.xpath("./text()"), str2)
+                    else:
+                        for link in links:
+                            add_triple(ont_name, link.xpath("./text()"), str2)
+
+                if not iss:
+                    links = li.xpath("./a")
+                    if not links:
+                        add_triple(ont_name, li.xpath("./text()"), str2)
+                    else:
+                        for link in links:
+                            add_triple(ont_name, link.xpath("./text()"), str2)
+        if not lis:
+
+            iss = td[0].xpath(".//i")
+            for i in iss:
+
+                links = i.xpath("./a")
+                if not links:
+                    add_triple(ont_name, i.xpath("./text()"), str2)
+                else:
+                    for link in links:
+                        add_triple(ont_name, link.xpath("./text()"), str2)
+            if not iss:
+                dvs=td[0].xpath("./div")
+                for dv in dvs:
+                    links = dv.xpath("./a")
+                    if not links:
+                        add_triple(ont_name, dv.xpath("./text()"), str2)
+                    else:
+                        for link in links:
+                            add_triple(ont_name, link.xpath("./text()"), str2)
+                if not dvs:
+                    links = td[0].xpath("./a")
+                    if not links:
+                        add_triple(ont_name, td[0].xpath("./text()"), str2)
+                    else:
+                        for link in links:
+                            add_triple(ont_name, link.xpath("./text()"), str2)
+
+
+def human_process(address):
+    res = requests.get('https://en.wikipedia.org/' + address[0])
+    print('https://en.wikipedia.org/' + address[0])
+    doc = lxml.html.fromstring(res.content)
+    a = doc.xpath("//table[contains(@class,'infobox')]")
+    if a:
+        name=a[0].xpath("./tbody/tr[1]//text()")
+        if name:
+            name = name[0].replace(" ", "_")
+            ont_name = rdflib.URIRef(f'{PREFIX}/{name}')
+            check_human(ont_name, a, 'Born', 'ont_born')
+            check_human(ont_name, a, 'Occupation', 'ont_occupation')
+            check_human(ont_name, a, 'Awards', 'ont_award')
+
+            g.serialize("ontology.nt", format="nt")
+
 # check for some possible structures of the infobox
-def check_cases(ont_name,a,str1,str2,g):
+def check_cases(ont_name,a,str1,str2):
 
     td = a[0].xpath("./tbody/tr[./th//text()='"+str1+"']/td")
 
@@ -30,7 +136,7 @@ def check_cases(ont_name,a,str1,str2,g):
             else:
                 for bday in bdays:
 
-                    add_triple(ont_name,bday.xpath("./text()"),str2,g)
+                    add_triple(ont_name,bday.xpath("./text()"),str2)
                 return
 
         elif str1=='Based on':
@@ -45,17 +151,25 @@ def check_cases(ont_name,a,str1,str2,g):
 
                 links = i.xpath("./a")
                 if not links:
-                    add_triple(ont_name, i.xpath("./text()"), str2,g)
+                    add_triple(ont_name, i.xpath("./text()"), str2)
                 else:
                     for link in links:
-                        add_triple(ont_name, link.xpath("./text()"), str2,g)
+                        add_triple(ont_name, link.xpath("./text()"), str2)
+                    if str1 in ['Directed by', 'Produced by', 'Starring']:
+                        for link in links:
+
+                            human_process(link.xpath("./@href"))
+
             if not iss:
                 links = li.xpath("./a")
                 if not links:
-                    add_triple(ont_name, li.xpath("./text()"), str2,g)
+                    add_triple(ont_name, li.xpath("./text()"), str2)
                 else:
                     for link in links:
-                        add_triple(ont_name, link.xpath("./text()"), str2,g)
+                        add_triple(ont_name, link.xpath("./text()"), str2)
+                    if str1 in ['Directed by', 'Produced by', 'Starring']:
+                        for link in links:
+                            human_process(link.xpath("./@href"))
         if not lis:
 
             iss = td[0].xpath(".//i")
@@ -63,32 +177,41 @@ def check_cases(ont_name,a,str1,str2,g):
 
                 links = i.xpath("./a")
                 if not links:
-                    add_triple(ont_name, i.xpath("./text()"), str2,g)
+                    add_triple(ont_name, i.xpath("./text()"), str2)
                 else:
                     for link in links:
-                        add_triple(ont_name, link.xpath("./text()"), str2,g)
+                        add_triple(ont_name, link.xpath("./text()"), str2)
+                    if str1 in ['Directed by', 'Produced by', 'Starring']:
+                        for link in links:
+                            human_process(link.xpath("./@href"))
             if not iss:
                 dvs=td[0].xpath("./div")
                 for dv in dvs:
                     links = dv.xpath("./a")
                     if not links:
-                        add_triple(ont_name, dv.xpath("./text()"), str2,g)
+                        add_triple(ont_name, dv.xpath("./text()"), str2)
                     else:
                         for link in links:
-                            add_triple(ont_name, link.xpath("./text()"), str2,g)
+                            add_triple(ont_name, link.xpath("./text()"), str2)
+                        if str1 in ['Directed by', 'Produced by', 'Starring']:
+                            for link in links:
+                                human_process(link.xpath("./@href"))
                 if not dvs:
                     links = td[0].xpath("./a")
                     if not links:
-                        add_triple(ont_name, td[0].xpath("./text()"), str2,g)
+                        add_triple(ont_name, td[0].xpath("./text()"), str2)
                     else:
                         for link in links:
-                            add_triple(ont_name, link.xpath("./text()"), str2,g)
+                            add_triple(ont_name, link.xpath("./text()"), str2)
+                        if str1 in ['Directed by', 'Produced by', 'Starring']:
+                            for link in links:
+                                human_process(link.xpath("./@href"))
 
 
 
 # process page by the six elements that are required to the questions (for the movies)
 # todo: process actors, film directors, etc.
-def process_page(address,g):
+def process_page(address):
     res=requests.get('https://en.wikipedia.org/'+address)
     print('https://en.wikipedia.org/'+address)
     doc = lxml.html.fromstring(res.content)
@@ -96,17 +219,17 @@ def process_page(address,g):
     name = a[0].xpath("./tbody/tr[1]//text()")[0].replace(" ","_")
 
     ont_name=rdflib.URIRef(f'{PREFIX}/{name}')
-    check_cases(ont_name,a,'Directed by','ont_directed',g)
-    check_cases(ont_name, a, 'Produced by', 'ont_produced',g)
-    check_cases(ont_name, a, 'Based on', 'ont_based',g)
-    check_cases(ont_name, a, 'Starring', 'ont_starring',g)
-    check_cases(ont_name, a, 'Release date', 'ont_release',g)
-    check_cases(ont_name, a, 'Running time', 'ont_running',g)
+    check_cases(ont_name,a,'Directed by','ont_directed')
+    check_cases(ont_name, a, 'Produced by', 'ont_produced')
+    check_cases(ont_name, a, 'Based on', 'ont_based')
+    check_cases(ont_name, a, 'Starring', 'ont_starring')
+    check_cases(ont_name, a, 'Release date', 'ont_release')
+    check_cases(ont_name, a, 'Running time', 'ont_running')
 
     g.serialize("ontology.nt",format="nt")
 
 # maybe there is a way to not pass g through all the calls
-def build_ontology(g):
+def build_ontology():
 
     ont_directed = rdflib.URIRef(f'{PREFIX}/directed_by')
     ont_produced = rdflib.URIRef(f'{PREFIX}/produced_by')
@@ -115,12 +238,20 @@ def build_ontology(g):
     ont_release = rdflib.URIRef(f'{PREFIX}/release_date')
     ont_running = rdflib.URIRef(f'{PREFIX}/running_time')
 
+    ont_born=rdflib.URIRef(f'{PREFIX}/born')
+    ont_occupation = rdflib.URIRef(f'{PREFIX}/occupation')
+    ont_award=rdflib.URIRef(f'{PREFIX}/award')
+
     relation_ontologies['ont_directed']=ont_directed
     relation_ontologies['ont_produced'] = ont_produced
     relation_ontologies['ont_based'] = ont_based
     relation_ontologies['ont_starring'] = ont_starring
     relation_ontologies['ont_release'] = ont_release
     relation_ontologies['ont_running'] = ont_running
+
+    relation_ontologies['ont_born'] = ont_born
+    relation_ontologies['ont_occupation'] = ont_occupation
+    relation_ontologies['ont_award'] = ont_award
 
     lst_of_address=[]
     res=requests.get(seed_address)
@@ -130,6 +261,6 @@ def build_ontology(g):
         lst_of_address.append(t)
 
     for address in lst_of_address:
-        process_page(address,g)
+        process_page(address)
         #process_page('/wiki/Amy_(2015_film)')
         #break
